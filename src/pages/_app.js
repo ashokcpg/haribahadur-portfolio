4355bcc1b7bceb21dd77d91db2197a7ecbec6396
styrene <styrene@styrene.styrene>
2021-06-16 22:34:00 +0545

    Added Title to the website

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
-import Theme from '../styles/theme';
+import Theme from "../styles/theme";
+// import Head from "next/document";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Theme>
-        <Component {...pageProps} />
-      </Theme>
-    </>
-  );
+	return (
+		<>
+			<title>Hari Bahadur's Portfolio | Hari Bahadur's Website</title>
+			<Theme>
+				<Component {...pageProps} />
+			</Theme>
+		</>
+	);
 }
- 
